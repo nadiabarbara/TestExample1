baeb169126fb96331a7a2fc1f92f59a3eb218e5b
nadiabarbara <nadiabarbara10@gmail.com>
2025-03-26 15:48:55 +0200

    add js.js

---
+++ assets/js.js
@@ -0,0 +1,12 @@
+// Task 1
+document.write("Nadia" );
+document.writeln("Burbara");
+
+// Task 2
+const myObj = {firstname:"Nadia", lastname:"Burbara"};
+console.log(myObj);
+
+// Task 3
+const myArr = ["Robert", "Ronaldo", "Dan"];
+console.log(myArr);
+
